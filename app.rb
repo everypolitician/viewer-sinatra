454022ec7cc5f11f6db79bda591dbc7a0a3f17e7
Tony Bowden <tony@tmtm.com>
2015-04-02 12:29:19 +0100

    Put the app back
    The Mac Github client decided to do the last change with a move

---
+++ app.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+require 'haml'
+require 'json'
+require 'popolo_helper'
+
+helpers Popolo::Helper
+
+get '/' do
+  haml :index
+end
+
+get '/about.html' do
+  haml :about
+end
+
+get '/terms.html' do
+  @terms = terms
+  haml :terms
+end
+
+get '/people.html' do
+  @people = persons
+  haml :people
+end
+
+get '/parties.html' do
+  @parties = parties
+  haml :parties
+end
+
+get '/term/:id' do |id|
+  @term = term_from_id(id) or pass
+  haml :term
+end
+
+get '/person/:id' do |id|
+  @person = person_from_id(id) or pass
+  haml :person
+end
+
+get '/party/:id' do |id|
+  @party = party_from_id(id) or pass
+  @memberships = party_memberships(@party['id'])
+  haml :party
+end
+
+
+
+
