2577396d18279e8a4f1e0bf603d6a7df350f4364
Tony Bowden <tony@tmtm.com>
2015-04-25 20:34:28 +0100

    Dynamically add `rake country` tasks
    Shell quoting makes calls to `rake regenerate[wales]` awkward, so
    dynamically create a task for each country instead.

--- data/Rakefile.rb
+++ data/Rakefile.rb
@@ -1,7 +1,17 @@
 ﻿
 @COUNTRIES = FileList['*/Rakefile.rb'].pathmap('%d')
 
-desc "Regenarate a specific country"
+def run_country
+  puts "Wahey!"
+end
+
+@COUNTRIES.each do |country|
+  desc "Regenerate #{country}"
+  task country.to_sym do 
+    Rake::Task[:regenerate].execute(country: country) 
+  end
+end
+
 task :regenerate, :country do |t, args|
   country = args[:country] or abort "Need a country"
   abort "Don't know how to build #{country}" unless @COUNTRIES.include? country
@@ -13,7 +23,7 @@ end
 desc "Regenarate all countries"
 task :regenerate_all do
   @COUNTRIES.each do |country| 
-    Rake::Task[:regenerate].execute(country: country) 
+    Rake::Task[country.to_sym].execute
   end
 end
 
