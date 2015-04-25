9cc9b5508034dcc8f3feb59bea3009eea70afea1
Tony Bowden <tony@tmtm.com>
2015-04-25 20:16:15 +0100

    Rakefile to trigger all other rake files
    cd into each country that has a Rakefile, and run it

---
+++ data/Rakefile.rb
@@ -0,0 +1,20 @@
+﻿
+@COUNTRIES = FileList['*/Rakefile.rb'].pathmap('%d')
+
+desc "Regenarate a specific country"
+task :regenerate, :country do |t, args|
+  country = args[:country] or abort "Need a country"
+  abort "Don't know how to build #{country}" unless @COUNTRIES.include? country
+  chdir country
+  sh 'rake rebuild'
+  chdir '..'
+end
+
+desc "Regenarate all countries"
+task :regenerate_all do
+  @COUNTRIES.each do |country| 
+    Rake::Task[:regenerate].execute(country: country) 
+  end
+end
+
+
