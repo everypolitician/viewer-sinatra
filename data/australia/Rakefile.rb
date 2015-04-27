d04dd81edb840b89586c0bdd1d4f489415312d2a
Tony Bowden <tony@tmtm.com>
2015-04-27 16:24:32 +0100

    Rewrite in keeping with the new Rakefile approach

--- data/australia/Rakefile.rb
+++ data/australia/Rakefile.rb
@@ -1,17 +1,10 @@
-require 'date'
-require 'fileutils'
-require 'json'
-require 'open-uri'
 require 'csv'
 require 'csv_to_popolo'
 
+require_relative '../rakefile_common.rb'
 
-# Numeric.class_eval { def empty?; false; end }
-
-file 'popit.json' do
-  POPIT_URL = 'https://australia.popit.mysociety.org/api/v0.1/export.json'
-  File.write('popit.json', open(POPIT_URL).read)
-end
+@DEST = 'australia'
+@JSON_FILE = 'fromcsv.json'
 
 file 'popit.csv' => 'popit.json' do
   data = JSON.load(File.read('popit.json'))['persons'].map do |p|
@@ -31,19 +24,24 @@ file 'popit.csv' => 'popit.json' do
   csv = data.first.keys.to_csv  + data.map { |r| r.values.to_csv }.join
   File.write('popit.csv', csv.gsub(',null',','))
 end
+CLEAN.include('popit.csv')
 
 file 'fromcsv.json' => 'popit.csv' do
   data = Popolo::CSV.new('popit.csv').data
   json = JSON.pretty_generate(data)
   File.write('fromcsv.json', json)
 end
+CLEAN.include('fromcsv.json')
 
-file 'processed.json' => 'fromcsv.json' do
-  json = JSON.parse(File.read('fromcsv.json'), symbolize_names: true)
+task :load_json => 'fromcsv.json'
+
+
+task :process_json => :load_json do
 
   # ensure there's a legislative Organization
-  if json[:organizations].find_all { |h| h[:classification] == 'legislature' }.count.zero?
-    json[:organizations] << {
+  binding.pry
+  if @json[:organizations].find_all { |h| h[:classification] == 'legislature' }.count.zero?
+    @json[:organizations] << {
       classification: "legislature",
       name: "Legislature",
       id: "legislature",
@@ -51,13 +49,13 @@ file 'processed.json' => 'fromcsv.json' do
   end
 
   # ensure the chambers are children of the legislature
-  json[:organizations].find_all { |h| h[:classification] == 'chamber' }.each do |c|
+  @json[:organizations].find_all { |h| h[:classification] == 'chamber' }.each do |c|
     c['parent_id'] ||= 'legislature'
   end
 
   # Add terms — Let's simply deal with Parliaments, ignoring the 6-year Senate
   # http://en.wikipedia.org/wiki/Chronology_of_Australian_federal_parliaments
-  leg = json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
+  leg = @json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
   unless leg.has_key?(:legislative_periods) and not leg[:legislative_periods].count.zero? 
     leg[:legislative_periods] = [{
       id: 'term/44',
@@ -67,23 +65,9 @@ file 'processed.json' => 'fromcsv.json' do
     }]
   end
 
-  json[:memberships].find_all { |m| m.has_key?(:on_behalf_of_id) and m[:role] == 'member' }.each do |m|
+  @json[:memberships].find_all { |m| m.has_key?(:on_behalf_of_id) and m[:role] == 'member' }.each do |m|
     m[:legislative_period_id] ||= 'term/current'
   end
   
-
-  File.write('processed.json', JSON.pretty_generate(json))
-end
-
-task :rebuild => [ :clean, 'processed.json' ]
-
-task :clean do
-  FileUtils.rm('processed.json') if File.exist?('processed.json')
-end
-
-task :default => 'processed.json'
-
-task :install => 'processed.json' do
-  FileUtils.cp('processed.json', '../australia.json')
 end
 
