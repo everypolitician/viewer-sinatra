ec1849a7c4cbf3367697a646f58a0e851a4bed39
Tony Bowden <tony@tmtm.com>
2015-06-25 15:53:05 +0100

    Inflate area_ids to full areas
    Just as we promise to inflate person_id and organization_id etc into
    objects, do the same with area_id

---
+++ t/web/term_table/malaysia.rb
@@ -0,0 +1,29 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../../../app'
+require 'minitest/autorun'
+require 'rack/test'
+require 'nokogiri'
+
+include Rack::Test::Methods
+
+def app
+  Sinatra::Application
+end
+
+describe 'Per Country Tests' do
+  subject { Nokogiri::HTML(last_response.body) }
+  let(:memtable) { subject.css('.term-membership-table') }
+
+  describe 'Malaysia' do
+    before { get '/malaysia/term_table/13.html' }
+
+    it 'should have have its name' do
+      subject.css('#term h1').text.must_include '13th Parliament of Malaysia'
+    end
+
+    it 'should list the areas' do
+      memtable.text.must_include 'Samarahan, Sarawak'
+    end
+  end
+end
