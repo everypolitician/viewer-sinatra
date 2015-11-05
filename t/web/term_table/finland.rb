03e7526fc52de2208c7767bc3b470e7782644596
Tony Bowden <tony@tmtm.com>
2015-11-05 18:03:49 +0000

    Switch from IDs to UUIDs in terms

--- t/web/term_table/finland.rb
+++ t/web/term_table/finland.rb
@@ -35,20 +35,20 @@ describe 'Per Country Tests' do
     end
 
     it "shouldn't show any dates for Mikko Kuoppa" do
-      memtable.css('tr#mem-444').text.wont_include '20'
+      memtable.css('tr#mem-44b4a448-f9a7-47f0-abbb-79858799f58f').text.wont_include '20'
     end
 
     it 'should show early departure date for Matti Vanhanen' do
-      memtable.css('tr#mem-414 td:last').text.must_include '2010-09-19'
+      memtable.css('tr#mem-5276efaf-c4e5-424b-ac69-571b74d80f8c td:last').text.must_include '2010-09-19'
     end
 
     it 'should show late start date for Risto Kuisma' do
-      memtable.css('tr#mem-473 td:last').text.must_include '2010-07-13'
+      memtable.css('tr#mem-7dd1ad94-d204-4d0f-aa3e-9762cbab7c9f td:last').text.must_include '2010-07-13'
     end
 
     it 'should have two rows for Merikukka Forsius' do
       # Changed Party mid-term, so one entry per party
-      memtable.at_css('tr#mem-560 td:first').attr('rowspan').to_i.must_equal 2
+      memtable.at_css('tr#mem-c1db6bc0-601c-4c8c-b8be-7b0898bacdbf td:first').attr('rowspan').to_i.must_equal 2
     end
 
     it 'should link to 34' do
--- t/web/term_table/ni.rb
+++ t/web/term_table/ni.rb
@@ -19,17 +19,17 @@ describe 'Northern Ireland' do
     before { get '/northern-ireland/assembly/term-table/3.html' }
 
     it 'should have no dates for Adrian McQuillan' do
-      subject.css('tr#mem-13912 td').last.text.must_be :empty?
+      subject.css('tr#mem-1074213b-e6f1-4427-bb85-176ab389a305 td').last.text.must_be :empty?
     end
 
     it 'should have a start date for Alastair Ross' do
-      subject.css('tr#mem-13927 td').last.text.must_include '2007-05-14'
-      subject.css('tr#mem-13927 td').last.text.wont_include '2011-03-24'
+      subject.css('tr#mem-c42dfdb8-647d-40db-98ce-894ca19b6aec td').last.text.must_include '2007-05-14'
+      subject.css('tr#mem-c42dfdb8-647d-40db-98ce-894ca19b6aec td').last.text.wont_include '2011-03-24'
     end
 
     it 'should have an end date for David Simpson' do
-      subject.css('tr#mem-11892 td').last.text.wont_include '2007-03-09'
-      subject.css('tr#mem-11892 td').last.text.must_include '2010-07-01'
+      subject.css('tr#mem-62479dcd-b981-4a57-8a11-b8f8b17249fb td').last.text.wont_include '2007-03-09'
+      subject.css('tr#mem-62479dcd-b981-4a57-8a11-b8f8b17249fb td').last.text.must_include '2010-07-01'
     end
 
   end
