7cdfdca585dd2d2e368da0e6ad811cd84c5ab75b
Chris Mytton <chrism@mysociety.org>
2016-03-22 17:06:03 +0100

    Add tests for Popolo::Helper class

---
+++ t/web/popolo_helper.rb
@@ -0,0 +1,18 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../../app'
+require 'minitest/autorun'
+
+describe Popolo::Helper do
+  include Popolo::Helper
+
+  describe '#number_to_millions' do
+    it 'formats numbers as millions to one decimal place' do
+      number_to_millions(2700000).must_equal(2.7)
+    end
+
+    it 'rounds down rather than up' do
+      number_to_millions(2680000).must_equal(2.6)
+    end
+  end
+end
