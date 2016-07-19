93a982c22d43ae94606c57c76ccbcfbeb34d35c6
Oliver Denman <oliver@therewillbeanimals.com>
2016-07-19 09:28:11 +0100

    Refactored. Event handler now added to all elements with .track-event class

--- public/javascript/ga-event-tracking.js
+++ public/javascript/ga-event-tracking.js
@@ -1,45 +1,15 @@
 $(document).ready(function() {
-	function trackEvent(str){
-		console.log("Tracking event: "+str)
-	}
-
-	$("#term-navigation__prev, #term-navigation__next, #term-table-download-button").click(function(event){
-		trackTermTableEvent(String(event.target.id))
-	})
-
-	// TODO: how the hell do I get the IDs of these?
-
-	$("#section-toggle-social").click(function(){
-		trackTermTableEvent('#section-toggle-social')
-	})
-
-	$("#section-toggle-bio").click(function(){
-		trackTermTableEvent('#section-toggle-social')
-	})
-
-	$("#section-toggle-contacts").click(function(event){
-		trackTermTableEvent('#section-toggle-contacts')
-	})
-
-	$("#section-toggle-identifiers").click(function(event){
-		trackTermTableEvent('#section-toggle-identifiers')
-	})
-
-	$('#term-navigation__menu').change(function() {
-		trackTermTableEvent('#term-navigation__menu')
-	})
-
-	// $('#filter-input').change(function() {
-	// 	alert('Filter box!')
-	// })
-
-	function trackTermTableEvent(label){
-		alert('Tracking event!')
-		// ga('send', {
-		//   hitType: 'event',
-		//   eventCategory: 'User interaction',
-		//   eventAction: 'click - term table',
-		//   eventLabel: label
-		// })
+	var elements = document.getElementsByClassName("track-event")
+	for (var i = 0; i < elements.length; i++) {
+		(function(i){
+			elements[i].addEventListener('click', function(){
+				trackEvent(elements[i].getAttribute('ga-description'))
+			})
+		})(i)
 	}
 })
+
+function trackEvent(eventDescription){
+	eventDescription = eventDescription || 'Undescribed event.'
+	ga('send', 'event', 'user interaction', eventDescription, document.title)
+}
