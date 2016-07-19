b7a0deb3954ef997ad9916bf94177ff9769008ea
Oliver Denman <oliver@therewillbeanimals.com>
2016-07-19 11:38:33 +0100

    Moved .trackClick outside of event handler

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -380,13 +380,13 @@ $(function(){
 
   // Event tracking
   // Clicks on elements with data-ga-track-click attribute will be recorded
+  function trackClick(eventDescription){
+    eventDescription = eventDescription || 'Undescribed event.';
+    ga('send', 'event', 'user interaction', eventDescription, document.title);
+  }
+  
   $('[data-ga-track-click]').on('click', function(){ 
     trackClick( $(this).attr('data-ga-track-click')); 
-
-    function trackClick(eventDescription){
-      eventDescription = eventDescription || 'Undescribed event.';
-      ga('send', 'event', 'user interaction', eventDescription, document.title);
-    }
   })
 
 });
