1af3357e002b05881a17bde2aa856fbbbc10c9a5
Tony Bowden <tony@tmtm.com>
2016-08-01 17:14:34 +0100

    Rubocop fixes for popolo_helper

--- lib/popolo_helper.rb
+++ lib/popolo_helper.rb
@@ -2,26 +2,21 @@ require 'open-uri'
 require 'fileutils'
 
 module EveryPolitician
-
   class GithubFile
-
-    @@GH_PATH = "https://cdn.rawgit.com/everypolitician/everypolitician-data/%s/%s"
+    @@GH_PATH = 'https://cdn.rawgit.com/everypolitician/everypolitician-data/%s/%s'
 
     def initialize(file, sha, cache_dir = '_cached_data')
       @url = @@GH_PATH % [sha, file]
 
       FileUtils.mkpath cache_dir
-      @cache_file = File.join cache_dir, sha + "-" + file.tr('/', '-')
+      @cache_file = File.join cache_dir, sha + '-' + file.tr('/', '-')
     end
 
-    def url
-      @url
-    end
+    attr_reader :url
 
     def raw
       @_data ||= begin
         unless File.exist? @cache_file
-          # puts "Writing #{@url} to #{@cache_file}"
           File.write @cache_file, open(@url).read
         end
         File.read(@cache_file)
@@ -31,21 +26,20 @@ module EveryPolitician
 end
 
 module Popolo
-
   module Helper
     def term_table_url(c, h, t)
-      "/%s/%s/term-table/%s.html" % [ c[:slug].downcase, h[:slug].downcase, t[:csv][/term-(.*?).csv/, 1] ]
+      '/%s/%s/term-table/%s.html' % [c[:slug].downcase, h[:slug].downcase, t[:csv][/term-(.*?).csv/, 1]]
     end
 
-    # http://stackoverflow.com/questions/1078347/is-there-a-rails-trick-to-adding-commas-to-large-numbers
+    # http://stackoverflow.com/questions/1078347/is-there-a-rails-trick-to-adding-commas-to-large-numbers
     def commify(number)
       number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
     end
 
     def wikidata_link(p)
       return if p[:identifiers].nil? || p[:identifiers].empty?
-      wd = p[:identifiers].find { |i| i[:scheme] == 'wikidata' } or return
-      '<a href="https://www.wikidata.org/wiki/%s">%s</a>' % [ wd[:identifier], wd[:identifier] ]
+      (wd = p[:identifiers].find { |i| i[:scheme] == 'wikidata' }) || return
+      '<a href="https://www.wikidata.org/wiki/%s">%s</a>' % [wd[:identifier], wd[:identifier]]
     end
 
     def image_proxy_url(id)
@@ -55,7 +49,7 @@ module Popolo
 
     def number_to_millions(num)
       result = (num.to_f / 100_000).floor.to_f / 10
-      result.modulo(1) < 0.1 ?  result.to_i : result
+      result.modulo(1) < 0.1 ? result.to_i : result
     end
   end
 end
