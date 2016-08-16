881e0a923bb98a676bf584a3604d86e9ce067e22
Tony Bowden <tony@tmtm.com>
2016-08-16 16:01:44 +0100

    Add descriptions to rake test tasks
    This makes the output of rake rubocop               # Run RuboCop
    rake rubocop:auto_correct  # Auto-correct RuboCop offenses
    rake test                  # Run "Page" tests
    rake test:all              # Run all tests (slow)
    rake test:web              # Run "Web" tests (slow) much more useful. Thanks to @chrismytton for the pointer to this.

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,6 +3,7 @@ require 'rubocop/rake_task'
 
 Rake::TestTask.new do |t|
   t.warning = true
+  t.description = 'Run "Page" tests'
   t.test_files = FileList['t/page/*.rb']
 end
 
@@ -10,12 +11,14 @@ Rake::TestTask.new do |t|
   t.name = 'test:web'
   t.warning = false
   t.verbose = true
+  t.description = 'Run "Web" tests (slow)'
   t.test_files = FileList['t/web/*.rb']
 end
 
 Rake::TestTask.new do |t|
   t.name = 'test:all'
   t.verbose = true
+  t.description = 'Run all tests (slow)'
   t.test_files = FileList['t/**/*.rb']
 end
 
