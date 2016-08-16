2f3dd6466f5241125f843560e4d240992b0a0b03
Tony Bowden <tony@tmtm.com>
2016-08-16 15:46:48 +0100

    Split the exhaustive web tests to a separate rake task
    Running the entire test suite takes forever because it has to make sure
    that every country works as a fully rendered page.
    
    If we split those out to a separate `rake test:web` target, and ensure
    that `rake test:all` runs all of them, then we can still run the full
    test suite when needed, but the day-to-day run of the Page tests can be
    lightning fast.

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,7 +2,21 @@ require 'rake/testtask'
 require 'rubocop/rake_task'
 
 Rake::TestTask.new do |t|
-  t.pattern = 't/**/*.rb'
+  t.warning = true
+  t.test_files = FileList['t/page/*.rb']
+end
+
+Rake::TestTask.new do |t|
+  t.name = 'test:web'
+  t.warning = false
+  t.verbose = true
+  t.test_files = FileList['t/web/*.rb']
+end
+
+Rake::TestTask.new do |t|
+  t.name = 'test:all'
+  t.verbose = true
+  t.test_files = FileList['t/**/*.rb']
 end
 
 RuboCop::RakeTask.new
