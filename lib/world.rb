6e2c619e50ea8a234c8653e2594e72cc7a84448d
Tony Bowden <tony@tmtm.com>
2016-08-17 09:59:25 +0100

    Let World return us a Country
    We want to be able to plug a Country from World into the wrapper
    template so that we can call `.name` and `.url` on it, so wrap the
    entries up in a simple Struct adapter.

--- lib/world.rb
+++ lib/world.rb
@@ -9,6 +9,17 @@ class World
     @_wjson ||= JSON.parse(File.read(file), symbolize_names: true)
   end
 
+  # super-simplistic adapter for the inner data. Over time it might make
+  # sense to properly extract this to its own class, but for now we just
+  # want to tidy the interface up a little so that this is substitutable
+  # for an `everypolitician-ruby` Country in a few well-defined places.
+
+  Country = Struct.new(:url, :name, :names)
+  def country(slug)
+    return unless found = as_json[slug.to_sym]
+    Country.new(slug, found[:displayName], found[:allNames])
+  end
+
   private
 
   attr_reader :file
