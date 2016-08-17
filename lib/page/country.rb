d71fec252b0eda497f3604b258e771f3cd8fc1c9
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-08-17 12:35:58 +0100

    Extract the title logic out of the constructor

--- lib/page/country.rb
+++ lib/page/country.rb
@@ -2,14 +2,16 @@ require 'everypolitician'
 
 module Page
   class Country
-    attr_reader :title
     def initialize(slug)
-      @slug    = slug
-      @title   = "EveryPolitician: #{country[:name]}" if country
+      @slug = slug
     end
 
     def country
       EveryPolitician.country(@slug)
+    end
+
+    def title
+      "EveryPolitician: #{country[:name]}" if country
     end
   end
 end
