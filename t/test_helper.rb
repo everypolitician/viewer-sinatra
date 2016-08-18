a95be291998eedb2a2fcfb6adc4948de8df22d51
Chris Mytton <chrism@mysociety.org>
2016-08-18 16:49:59 +0100

    Fix rubocop offence

--- t/test_helper.rb
+++ t/test_helper.rb
@@ -1,11 +1,13 @@
 require 'minitest/autorun'
 
-class Minitest::Spec
-  def known_countries_json_url
-    'https://cdn.rawgit.com/everypolitician/everypolitician-data/d8a4682f/countries.json'
-  end
+module Minitest
+  class Spec
+    def known_countries_json_url
+      'https://cdn.rawgit.com/everypolitician/everypolitician-data/d8a4682f/countries.json'
+    end
 
-  def index_at_known_sha
-    @index_at_known_sha ||= EveryPolitician::Index.new(index_url: known_countries_json_url)
+    def index_at_known_sha
+      @index_at_known_sha ||= EveryPolitician::Index.new(index_url: known_countries_json_url)
+    end
   end
 end
