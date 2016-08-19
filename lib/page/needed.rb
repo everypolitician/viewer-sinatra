d14af896b2ecb1b0c1ec1724a032eb2909269ef9
Tony Bowden <tony@tmtm.com>
2016-08-19 21:05:54 +0100

    extract 'issues' method
    remove some duplication in the calls to client.issues

--- lib/page/needed.rb
+++ lib/page/needed.rb
@@ -7,20 +7,24 @@ module Page
     end
 
     def to_find
-      client.issues 'everypolitician/everypolitician-data', labels: 'New Country,To Find'
+      issues 'New Country,To Find'
     end
 
     def to_scrape
-      client.issues 'everypolitician/everypolitician-data', labels: 'New Country,To Scrape'
+      issues 'New Country,To Scrape'
     end
 
     def to_finish
-      client.issues 'everypolitician/everypolitician-data', labels: 'New Country,3 - WIP'
+      issues 'New Country,3 - WIP'
     end
 
     private
 
     attr_reader :access_token
+
+    def issues(labels)
+      client.issues 'everypolitician/everypolitician-data', labels: labels
+    end
 
     def client
       client = Octokit::Client.new(access_token: @access_token)
