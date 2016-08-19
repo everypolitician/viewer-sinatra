93b1772137c6dae7cb484b0880ef4b924e629e96
Chris Mytton <chrism@mysociety.org>
2016-08-19 16:11:36 +0100

    Use VCR to record all http requests in the tests

--- t/test_helper.rb
+++ t/test_helper.rb
@@ -1,13 +1,26 @@
 # frozen_string_literal: true
 require 'minitest/autorun'
-require 'webmock/minitest'
-WebMock.allow_net_connect!
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 't/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('<GITHUB_ACCESS_TOKEN>') { ENV['GITHUB_ACCESS_TOKEN'] }
+end
+
+# FIXME: This should be within a test class, but it needs to be here because
+# countries.json is fetched and parsed at boot time in app.rb.
+WebMock.stub_request(:get, %r{https://cdn.rawgit.com/everypolitician/everypolitician-data/\w+?/countries.json})
+       .to_return(body: File.read('t/fixtures/d8a4682f-countries.json'))
 
 module Minitest
   class Spec
-    def setup
-      stub_request(:get, 'https://cdn.rawgit.com/everypolitician/everypolitician-data/d8a4682f/countries.json')
-        .to_return(body: File.read('t/fixtures/d8a4682f-countries.json'))
+    before do
+      VCR.insert_cassette(name)
+    end
+
+    after do
+      VCR.eject_cassette
     end
 
     def known_countries_json_url
