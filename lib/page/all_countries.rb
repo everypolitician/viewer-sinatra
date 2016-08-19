65234dbc3ebcca8425b5c4769ac9cb48557573f0
Tony Bowden <tony@tmtm.com>
2016-08-19 11:55:04 +0100

    don't do work in the initializer
    Keep the initialiser free of logic, and move the set-up of a new World
    to someone asking for it

--- lib/page/all_countries.rb
+++ lib/page/all_countries.rb
@@ -2,9 +2,8 @@ require_relative '../world.rb'
 
 module Page
   class AllCountries
-    attr_accessor :world
-    def initialize
-      @world = World.new.as_json
+    def world
+      World.new.as_json
     end
   end
 end
