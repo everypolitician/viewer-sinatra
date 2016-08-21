e92d5de1090a43f9fe4d3ba98f77e043d455515e
Tony Bowden <tony@tmtm.com>
2016-08-21 10:41:12 +0100

    rubocop tidying

--- t/test_helper.rb
+++ t/test_helper.rb
@@ -2,14 +2,17 @@
 ENV['RACK_ENV'] = 'test'
 
 require 'minitest/autorun'
-require 'vcr'
-require 'rack/test'
 require 'nokogiri'
+require 'pathname'
+require 'rack/test'
+require 'vcr'
 
 VCR.configure do |config|
   config.cassette_library_dir = 't/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('<GITHUB_ACCESS_TOKEN>') { ENV['GITHUB_ACCESS_TOKEN'] }
+  config.filter_sensitive_data('<GITHUB_ACCESS_TOKEN>') do
+    ENV['GITHUB_ACCESS_TOKEN']
+  end
 end
 
 module Minitest
@@ -21,8 +24,9 @@ module Minitest
     end
 
     before do
-      WebMock.stub_request(:get, %r{https://cdn.rawgit.com/everypolitician/everypolitician-data/\w+?/countries.json})
-             .to_return(body: File.read('t/fixtures/d8a4682f-countries.json'))
+      cj_file = %r{#{cdn}/#{ep_repo}/\w+/countries.json}
+      fixture = Pathname.new('t/fixtures/d8a4682f-countries.json')
+      WebMock.stub_request(:get, cj_file) .to_return(body: fixture.read)
       VCR.insert_cassette(name)
     end
 
@@ -30,12 +34,22 @@ module Minitest
       VCR.eject_cassette
     end
 
-    def known_countries_json_url
-      'https://cdn.rawgit.com/everypolitician/everypolitician-data/d8a4682f/countries.json'
+    def index_at_known_sha
+      @shaidx ||= EveryPolitician::Index.new(index_url: countries_json_url)
     end
 
-    def index_at_known_sha
-      @index_at_known_sha ||= EveryPolitician::Index.new(index_url: known_countries_json_url)
+    private
+
+    def cdn
+      'https://cdn.rawgit.com'
+    end
+
+    def ep_repo
+      'everypolitician/everypolitician-data'
+    end
+
+    def countries_json_url
+      URI.join(cdn, ep_repo + '/', 'd8a4682f/countries.json').to_s
     end
   end
 end
