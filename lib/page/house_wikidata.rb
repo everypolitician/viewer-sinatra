cec931668d6f00f47257447401609c71ba0ce0ab
Oliver Denman <oliver@therewillbeanimals.com>
2016-08-22 11:13:23 +0100

    Display members with and without wikidata info

--- lib/page/house_wikidata.rb
+++ lib/page/house_wikidata.rb
@@ -20,13 +20,24 @@ module Page
       country.legislature(house_slug)
     end
 
-    # TODO: we shouldn't be passing raw Popolo, only what's needed
-    def popolo
-      house.popolo
+    def people_with_wikidata
+      people_with_and_without_wikidata.first
+    end
+
+    def people_without_wikidata
+      people_with_and_without_wikidata.last
     end
 
     private
 
     attr_reader :house_slug, :country_slug, :index
+
+    def popolo
+      house.popolo
+    end
+
+    def people_with_and_without_wikidata
+      @with_and_without_array ||= popolo.persons.partition(&:wikidata)
+    end
   end
 end
