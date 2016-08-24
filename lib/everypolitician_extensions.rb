e2c7598696c7caf83e27d01cd4c23c5c61199513
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-08-24 12:24:47 +0100

    Remove the GitHubFile class from the helper
    Since this class is not needed anymore, as EP-ruby and
    EP-popolo libraries already provide that functionality, this
    class is not used anywhere at this point.

--- lib/everypolitician_extensions.rb
+++ lib/everypolitician_extensions.rb
@@ -9,28 +9,6 @@ module EveryPolitician
       legislatures.map(&:person_count).inject(:+)
     end
   end
-
-  class GithubFile
-    GH_PATH = 'https://cdn.rawgit.com/everypolitician/everypolitician-data/%s/%s'
-
-    # TODO: investigate whether we can remove this caching
-    # (possibly easier to just remove this entire class)
-    def initialize(file, sha, cache_dir = '_cached_data')
-      @url = GH_PATH % [sha, file]
-
-      FileUtils.mkpath cache_dir
-      @cache_file = File.join cache_dir, sha + '-' + file.tr('/', '-')
-    end
-
-    attr_reader :url
-
-    def raw
-      @_data ||= begin
-        File.write(@cache_file, open(@url).read) unless File.exist? @cache_file
-        File.read(@cache_file)
-      end
-    end
-  end
 end
 
 EveryPolitician::Country.include EveryPolitician::CountryExtension
