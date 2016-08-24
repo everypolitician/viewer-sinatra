8ab3aa71cdf40ec94af58e6cec8d8d43a9156e92
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-08-24 18:14:43 +0100

    Add temporary methods to Person in the helper

--- lib/everypolitician_extensions.rb
+++ lib/everypolitician_extensions.rb
@@ -31,6 +31,80 @@ module EveryPolitician
       end
     end
   end
+
+  module PersonExtension
+    def social
+      social = []
+      if twitter
+        social << {
+          type:  'Twitter',
+          value: "@#{twitter}",
+          link:  "https://twitter.com/#{twitter}",
+        }
+      end
+
+      if facebook
+        fb_username = URI.decode_www_form_component(facebook.split('/').last)
+        social << {
+          type:  'Facebook',
+          value: fb_username,
+          link:  "https://facebook.com/#{fb_username}",
+        }
+      end
+      social
+    end
+
+    def bio
+      bio = []
+      bio << { type: 'Gender', value: gender } if gender
+      bio << { type: 'Born', value: birth_date } if birth_date
+      bio << { type: 'Died', value: death_date } if death_date
+      bio << { type: 'Prefix', value: honorific_prefix } if honorific_prefix
+      bio << { type: 'Suffix', value: honorific_suffix } if honorific_suffix
+      bio
+    end
+
+    def contacts
+      contacts = []
+      contacts << { type: 'Email', value: email, link: "mailto:#{email}" }
+      contacts << { type: 'Phone', value: phone } if phone && email
+      contacts << { type: 'Fax', value: fax } if fax && email
+      contacts
+    end
+
+    def person_identifiers(people)
+      person_identifiers = []
+      if email
+        top_identifiers(people).each do |scheme|
+          id = identifiers.find { |i| i[:scheme] == scheme }
+          next if id.nil?
+          identifier = { type: id[:scheme], value: id[:identifier] }
+          if identifier[:type] == 'wikidata'
+            identifier[:link] = "https://www.wikidata.org/wiki/#{id[:identifier]}"
+          elsif identifier[:type] == 'viaf'
+            identifier[:link] = "https://viaf.org/viaf/#{id[:identifier]}/"
+          end
+          person_identifiers << identifier
+        end
+      end
+      person_identifiers
+    end
+
+    private
+
+    def top_identifiers(people)
+      @tidx ||= people
+                .map(&:identifiers)
+                .compact
+                .flatten
+                .reject { |i| i[:scheme] == 'everypolitician_legacy' }
+                .group_by { |i| i[:scheme] }
+                .sort_by { |_s, ids| -ids.size }
+                .map { |s, _ids| s }
+                .take(3)
+    end
+  end
 end
 
 EveryPolitician::Country.include EveryPolitician::CountryExtension
+EveryPolitician::Popolo::Person.include EveryPolitician::PersonExtension
