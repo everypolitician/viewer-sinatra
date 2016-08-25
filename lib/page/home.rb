f0270c68f084e18f172d241264a8140d675671f1
Tony Bowden <tony@tmtm.com>
2016-08-25 17:30:04 +0100

    Rejig the Homepage class
    combine World and EveryPolitician::Index, so that we have separate
    lists of "all countries" and "countries.data"

--- lib/page/home.rb
+++ lib/page/home.rb
@@ -9,11 +9,7 @@ module Page
     end
 
     def title
-      "EveryPolitician: Political data for #{countries.count} countries"
-    end
-
-    def countries
-      index.countries
+      "EveryPolitician: Political data for #{countries_with_data.count} countries"
     end
 
     def total_people
@@ -24,10 +20,12 @@ module Page
       legislatures.map(&:statement_count).inject(:+)
     end
 
-    def world
-      world_json.each do |slug, country|
-        country[:totalPeople] = index.country(slug.to_s).person_count rescue 0
-      end
+    def all_countries
+      world.countries
+    end
+
+    def countries_with_data
+      all_countries.map(&:epcountry).compact
     end
 
     private
@@ -35,11 +33,11 @@ module Page
     attr_reader :index
 
     def legislatures
-      @_legislatures ||= countries.flat_map(&:legislatures)
+      @_legislatures ||= countries_with_data.flat_map(&:legislatures)
     end
 
-    def world_json
-      World.new.as_json
+    def world
+      World.new(index: index)
     end
   end
 end
