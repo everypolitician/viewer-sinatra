305c4ae7e88e07193970146ab3b297848f31803d
Oliver Denman <oliver@therewillbeanimals.com>
2016-08-26 16:59:02 +0100

    Add #contact_info
    Method replaces #email, #fax, and #phone methods. Allows #entries result to contain multiple entries for each method of contact.

--- lib/subviews/person_contacts.rb
+++ lib/subviews/person_contacts.rb
@@ -1,31 +1,17 @@
 # frozen_string_literal: true
 class PersonContacts < PersonCard
   def entries
-    remove_entries_with_nil_values(
-      [email, phone, fax]
-    ).compact
+    email = contact_info type: 'email', label: 'Email'
+    phone = contact_info type: 'phone', label: 'Phone'
+    fax = contact_info type: 'fax', label: 'Fax'
+    remove_entries_with_nil_values([email, phone, fax].flatten)
   end
 
   private
 
-  def email
-    {
-      label: 'Email',
-      value: person.email,
-    }
-  end
-
-  def phone
-    {
-      label: 'Phone',
-      value: person.phone,
-    }
-  end
-
-  def fax
-    {
-      label: 'Fax',
-      value: person.fax,
-    }
+  def contact_info(type:, label:)
+    person.contact_details
+          .select { |i| i[:type] == type }
+          .map { |i| { label: label, value: i[:value] } }
   end
 end
--- t/subviews/person_contacts.rb
+++ t/subviews/person_contacts.rb
@@ -26,3 +26,19 @@ describe 'Social Subview' do
     subject.entries.must_include fax
   end
 end
+
+describe 'multiple entries' do
+  subject do
+    PersonContacts.new(person: index_at_known_sha.country('bulgaria').legislature('national-assembly')
+    .popolo
+    .persons
+    .select { |p| p.name == 'ВЕНЦИСЛАВ АСЕНОВ ЛАКОВ' }.first)
+  end
+
+  it 'should list multiple email addresses' do
+    email_one = { label: 'Email', value: 'vencilakov@abv.bg' }
+    email_two = { label: 'Email', value: 'ventsislav.lakov@parliament.bg' }
+    subject.entries.must_include email_one
+    subject.entries.must_include email_two
+  end
+end
