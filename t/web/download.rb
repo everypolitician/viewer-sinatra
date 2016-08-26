04523f62bc396f1108902c5bbe361daebdc13d13
Tony Bowden <tony@tmtm.com>
2016-08-26 12:01:25 +0100

    Tidy tests a little

--- t/web/download.rb
+++ t/web/download.rb
@@ -5,9 +5,19 @@ require_relative '../../app'
 describe 'Download names.csv button' do
   subject { Nokogiri::HTML(last_response.body) }
   before { get '/united-states-of-america/download.html' }
-  it 'should link to the names.csv file' do
-    subject_buttons = subject.css('a.button--download/@href').map(&:value).select { |h| h.include? '/names.csv' }
-    subject_buttons.must_include 'https://cdn.rawgit.com/everypolitician/everypolitician-data/735c68a/data/United_States_of_America/House/names.csv'
-    subject_buttons.must_include 'https://cdn.rawgit.com/everypolitician/everypolitician-data/cb4a755/data/United_States_of_America/Senate/names.csv'
+
+  let(:links) do
+    subject.css('a.button--download/@href')
+           .map(&:value)
+           .select { |h| h.include? '/names.csv' }
+           .join
+  end
+
+  it 'should link to the House names.csv file' do
+    links.must_include '735c68a/data/United_States_of_America/House/names.csv'
+  end
+
+  it 'should link to the Senate names.csv file' do
+    links.must_include 'cb4a755/data/United_States_of_America/Senate/names.csv'
   end
 end
