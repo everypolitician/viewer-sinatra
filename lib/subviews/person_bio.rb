c3ef5eebadb03e6c9a1bb53f62119edf7f5cf2cb
Oliver Denman <oliver@therewillbeanimals.com>
2016-08-26 11:29:07 +0100

    Add PersonBio subview class

---
+++ lib/subviews/person_bio.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+require_relative './person_card'
+class PersonBio < PersonCard
+  def entries
+    [
+      gender, birth_date,
+      death_date,
+      honorific_prefix,
+      honorific_suffix,
+    ].compact
+  end
+
+  private
+
+  def gender
+    { label: 'Gender',
+      value: person.gender, } if person.gender
+  end
+
+  def birth_date
+    { label: 'Born',
+      value: person.birth_date, } if person.birth_date
+  end
+
+  def death_date
+    { label: 'Died',
+      value: person.death_date, } if person.death_date
+  end
+
+  def honorific_prefix
+    { label: 'Prefix',
+      value: person.honorific_prefix, } if person.honorific_prefix
+  end
+
+  def honorific_suffix
+    { label: 'Suffix',
+      value: person.honorific_suffix, } if person.honorific_suffix
+  end
+end
---
+++ t/subviews/person_bio.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+require 'test_helper'
+require_relative '../../lib/subviews/person_bio'
+require 'everypolitician'
+
+describe 'Bio Subview: gender, birth and death date' do
+  subject do
+    PersonBio.new(person: index_at_known_sha.country('uk').legislature('commons')
+    .popolo
+    .persons
+    .select { |p| p.name == 'Harry Harpham' }.first)
+  end
+
+  it 'should list the birth date of a member' do
+    born = { label: 'Born', value: '1954-02-21' }
+    subject.entries.must_include born
+  end
+
+  it 'should list the death date of a member' do
+    died = { label: 'Died', value: '2016-02-04' }
+    subject.entries.must_include died
+  end
+
+  it 'should list the gender of a member' do
+    gender = { label: 'Gender', value: 'male' }
+    subject.entries.must_include gender
+  end
+end
+
+describe 'Bio Subview: honorific suffix' do
+  subject do
+    PersonBio.new(person: index_at_known_sha.country('uk').legislature('commons')
+    .popolo
+    .persons
+    .select { |p| p.name == 'Dominic Grieve' }.first)
+  end
+
+  it 'should list the honorific suffix of a member' do
+    suffix = { label: 'Suffix', value: 'Queen\'s Counsel' }
+    subject.entries.must_include suffix
+  end
+end
+
+describe 'Bio Subview: honorific prefix' do
+  subject do
+    PersonBio.new(person: index_at_known_sha.country('uk').legislature('commons')
+    .popolo
+    .persons
+    .select { |p| p.name == 'Margaret Beckett' }.first)
+  end
+
+  it 'should list the honorific suffix of a member' do
+    prefix = { label: 'Prefix', value: 'The Right Honourable' }
+    subject.entries.must_include prefix
+  end
+end
