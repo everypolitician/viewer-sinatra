2690e1dee14328179b2debee93fea0f561fa7038
Oliver Denman <oliver@therewillbeanimals.com>
2016-08-26 14:20:25 +0100

    Add :identifiers argument
    A list of identifier schemes is passed to the object. #entries returns a list of identifiers whose schemes are listed in the list.

--- lib/subviews/person_identifiers.rb
+++ lib/subviews/person_identifiers.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 class PersonIdentifiers < PersonCard
+  def initialize(person:, identifiers:)
+    @person = person
+    @identifiers = identifiers
+  end
+
   def entries
-    person.identifiers.map do |i|
+    arr = person.identifiers.map do |i|
       {
         label: i[:scheme],
         value: i[:identifier],
-      }
+      } if identifiers.include? i[:scheme]
     end
+    arr.compact
   end
+
+  private
+
+  attr_reader :identifiers
 end
--- t/subviews/person_identifiers.rb
+++ t/subviews/person_identifiers.rb
@@ -8,16 +8,16 @@ describe 'Identifiers Subview' do
     PersonIdentifiers.new(person: index_at_known_sha.country('uk').legislature('commons')
     .popolo
     .persons
-    .select { |p| p.name == 'Sarah Champion' }.first)
+    .select { |p| p.name == 'Sarah Champion' }.first, identifiers: %w(datadotparl parliamentdotuk))
   end
 
-  it 'should provide a list of Identifiers' do
+  it 'should provide provide a list of identifiers with schemes specified in the identifiers argument' do
     datadotparl = { label: 'datadotparl', value: '4267' }
     dods = { label: 'dods', value: '101347' }
     parliamentdotuk = { label: 'parliamentdotuk', value: 'commons/sarah-champion/4267' }
 
-    subject.entries.must_include datadotparl
-    subject.entries.must_include dods
+    subject.entries.wont_include dods
     subject.entries.must_include parliamentdotuk
+    subject.entries.must_include datadotparl
   end
 end
