4dcea936002f81b02aecf55e64c23e73ded13a1a
Oliver Denman <oliver@therewillbeanimals.com>
2016-08-26 11:42:04 +0100

    Add PersonContacts subview class

---
+++ lib/subviews/person_contacts.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+class PersonContacts < PersonCard
+  def entries
+    [email, phone, fax].flatten
+  end
+
+  private
+
+  def email
+    {
+      label: 'Email',
+      value: person.email,
+    } if person.email
+  end
+
+  def phone
+    {
+      label: 'Phone',
+      value: person.phone,
+    } if person.phone
+  end
+
+  def fax
+    {
+      label: 'Fax',
+      value: person.fax,
+    } if person.fax
+  end
+end
---
+++ t/subviews/person_contacts.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require 'test_helper'
+require_relative '../../lib/subviews/person_contacts'
+require 'everypolitician'
+
+describe 'Social Subview' do
+  subject do
+    PersonContacts.new(person: index_at_known_sha.country('uk').legislature('commons')
+    .popolo
+    .persons
+    .select { |p| p.name == 'David Anderson' }.first)
+  end
+
+  it 'should list an email address' do
+    email = { label: 'Email', value: 'andersonda@parliament.uk'}
+    subject.entries.must_include email
+  end
+
+  it 'should list a phone number' do
+    phone = { label: 'Phone', value: '020 7219 4348'}
+    subject.entries.must_include phone
+  end
+
+  it 'should list a fax number' do
+    fax = { label: 'Fax', value: '020 7219 8276'}
+    subject.entries.must_include fax
+  end
+end
