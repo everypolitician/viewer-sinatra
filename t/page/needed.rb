a89dac2387fc0e82e21f7ec0d4b961e2a8bca083
Chris Mytton <chrism@mysociety.org>
2016-08-26 16:06:00 +0100

    Remove VCR from the tests

--- t/page/needed.rb
+++ t/page/needed.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 require_relative '../../lib/page/needed'
-require 'minitest/autorun'
-require 'vcr'
+require 'test_helper'
 
 describe 'Needed' do
   subject do
--- t/test_helper.rb
+++ t/test_helper.rb
@@ -5,16 +5,7 @@ require 'minitest/autorun'
 require 'nokogiri'
 require 'pathname'
 require 'rack/test'
-require 'vcr'
 require 'pry'
-
-VCR.configure do |config|
-  config.cassette_library_dir = 't/fixtures/vcr_cassettes'
-  config.hook_into :webmock
-  config.filter_sensitive_data('<GITHUB_ACCESS_TOKEN>') do
-    ENV['GITHUB_ACCESS_TOKEN']
-  end
-end
 
 module Minitest
   class Spec
@@ -28,11 +19,6 @@ module Minitest
       cj_file = %r{#{cdn}/#{ep_repo}/\w+/countries.json}
       fixture = Pathname.new('t/fixtures/d8a4682f-countries.json')
       WebMock.stub_request(:get, cj_file) .to_return(body: fixture.read)
-      VCR.insert_cassette(name)
-    end
-
-    after do
-      VCR.eject_cassette
     end
 
     def index_at_known_sha
