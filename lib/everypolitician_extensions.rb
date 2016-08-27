5210405d6c83686b5be389493eb8344f4016d57f
Tony Bowden <tony@tmtm.com>
2016-08-27 05:24:15 +0100

    Extend EP-Legislature with a `term` method
    So people can call `.country("UK").legislature("Commons").term(65)`
    
    This should almost certainly move to everypolitician-ruby, but it's a
    little tricky as a _proper_ version will need to be able to navigate the
    boundaries between `everypolitician-ruby` and `everypolitician-popolo`
    (e.g. to get the members in that term). I'm not quite sure yet how
    that's going to work, so let's explore the issues here first.

--- lib/everypolitician_extensions.rb
+++ lib/everypolitician_extensions.rb
@@ -9,6 +9,16 @@ module EveryPolitician
       legislatures.map(&:person_count).inject(:+)
     end
   end
+
+  module LegislatureExtension
+    # UK.legislature('Commons').term(65)
+    def term(termid)
+      legislative_periods.find do |t|
+        t.id.split('/').last == termid.to_s
+      end
+    end
+  end
 end
 
 EveryPolitician::Country.include EveryPolitician::CountryExtension
+EveryPolitician::Legislature.include EveryPolitician::LegislatureExtension
