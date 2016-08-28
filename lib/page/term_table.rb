851a48e4b94c4851ea0dc1ab3aabf472cf8cdc24
Tony Bowden <tony@tmtm.com>
2016-08-28 08:01:49 +0100

    move the PersonCards into their own namespace

--- lib/page/term_table.rb
+++ lib/page/term_table.rb
@@ -62,10 +62,10 @@ module Page
           image:       person.image,
           proxy_image: image_proxy_url(person.id),
           memberships: person_memberships(person),
-          social:      SocialCard.new(person).data,
-          bio:         BioCard.new(person).data,
-          contacts:    ContactsCard.new(person).data,
-          identifiers: IdentifiersCard.new(person).data(top_identifiers),
+          social:      PersonCard::Social.new(person).data,
+          bio:         PersonCard::Bio.new(person).data,
+          contacts:    PersonCard::Contacts.new(person).data,
+          identifiers: PersonCard::Identifiers.new(person).data(top_identifiers),
         }
 
         p
@@ -146,69 +146,71 @@ module Page
   end
 end
 
-class PersonCard
-  def initialize(person)
-    @person = person
-  end
-
-  private
-
-  attr_reader :person
-end
-
-class SocialCard < PersonCard
-  def data
-    social_data = []
-
-    if person.twitter
-      social_data << { type: 'Twitter', value: "@#{person.twitter}", link: "https://twitter.com/#{person.twitter}" }
+module PersonCard
+  class Base
+    def initialize(person)
+      @person = person
     end
 
-    if person.facebook
-      fb_username = URI.decode_www_form_component(person.facebook.split('/').last)
-      social_data << { type: 'Facebook', value: fb_username, link: "https://facebook.com/#{fb_username}" }
-    end
+    private
 
-    social_data
+    attr_reader :person
   end
-end
 
-class ContactsCard < PersonCard
-  def data
-    contacts = []
-    contacts << { type: 'Email', value: person.email, link: "mailto:#{person.email}" } if person.email
-    contacts << { type: 'Phone', value: person.phone } if person.phone
-    contacts << { type: 'Fax', value: person.fax } if person.fax
-    contacts
-  end
-end
+  class Social < Base
+    def data
+      social_data = []
 
-class BioCard < PersonCard
-  def data
-    bio = []
-    bio << { type: 'Gender', value: person.gender } if person.gender
-    bio << { type: 'Born', value: person.birth_date } if person.birth_date
-    bio << { type: 'Died', value: person.death_date } if person.death_date
-    bio << { type: 'Prefix', value: person.honorific_prefix } if person.honorific_prefix
-    bio << { type: 'Suffix', value: person.honorific_suffix } if person.honorific_suffix
-    bio
-  end
-end
-
-class IdentifiersCard < PersonCard
-  def data(top_identifiers)
-    identifiers = []
-    top_identifiers.each do |scheme|
-      id = person.identifiers.find { |i| i[:scheme] == scheme }
-      next if id.nil?
-      identifier = { type: id[:scheme], value: id[:identifier] }
-      if identifier[:type] == 'wikidata'
-        identifier[:link] = "https://www.wikidata.org/wiki/#{id[:identifier]}"
-      elsif identifier[:type] == 'viaf'
-        identifier[:link] = "https://viaf.org/viaf/#{id[:identifier]}/"
+      if person.twitter
+        social_data << { type: 'Twitter', value: "@#{person.twitter}", link: "https://twitter.com/#{person.twitter}" }
       end
-      identifiers << identifier
+
+      if person.facebook
+        fb_username = URI.decode_www_form_component(person.facebook.split('/').last)
+        social_data << { type: 'Facebook', value: fb_username, link: "https://facebook.com/#{fb_username}" }
+      end
+
+      social_data
     end
-    identifiers
+  end
+
+  class Contacts < Base
+    def data
+      contacts = []
+      contacts << { type: 'Email', value: person.email, link: "mailto:#{person.email}" } if person.email
+      contacts << { type: 'Phone', value: person.phone } if person.phone
+      contacts << { type: 'Fax', value: person.fax } if person.fax
+      contacts
+    end
+  end
+
+  class Bio < Base
+    def data
+      bio = []
+      bio << { type: 'Gender', value: person.gender } if person.gender
+      bio << { type: 'Born', value: person.birth_date } if person.birth_date
+      bio << { type: 'Died', value: person.death_date } if person.death_date
+      bio << { type: 'Prefix', value: person.honorific_prefix } if person.honorific_prefix
+      bio << { type: 'Suffix', value: person.honorific_suffix } if person.honorific_suffix
+      bio
+    end
+  end
+
+  class Identifiers < Base
+    def data(top_identifiers)
+      identifiers = []
+      top_identifiers.each do |scheme|
+        id = person.identifiers.find { |i| i[:scheme] == scheme }
+        next if id.nil?
+        identifier = { type: id[:scheme], value: id[:identifier] }
+        if identifier[:type] == 'wikidata'
+          identifier[:link] = "https://www.wikidata.org/wiki/#{id[:identifier]}"
+        elsif identifier[:type] == 'viaf'
+          identifier[:link] = "https://viaf.org/viaf/#{id[:identifier]}/"
+        end
+        identifiers << identifier
+      end
+      identifiers
+    end
   end
 end
