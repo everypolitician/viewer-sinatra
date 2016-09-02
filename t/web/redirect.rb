8dfc544f25ac79236e23c3ce7c051c2cac9dcc7c
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-09-02 20:53:03 +0100

    Add tests for the redirect to a country page

--- t/web/redirect.rb
+++ t/web/redirect.rb
@@ -21,3 +21,23 @@ describe 'when hitting a docs page' do
     subject.css('.button--secondary/@href').text.must_equal url
   end
 end
+
+describe 'when hitting a country/download.html page' do
+  let(:url) { 'http://everypolitician.org/panama/' }
+  subject   { Nokogiri::HTML(last_response.body) }
+  before    { get '/panama/download.html' }
+
+  it 'should redirect to the right url' do
+    subject.css('meta/@content').first.text.must_equal "0; url=#{url}"
+    subject.css('link/@href').first.text.must_equal url
+  end
+
+  it 'should show the page title' do
+    subject.css('.hero--jazzy h1').text.must_equal 'EveryPolitician: Panama'
+  end
+
+  it 'should link to the right page' do
+    subject.css('.lead a/@href').text.must_equal url
+    subject.css('.button--secondary/@href').text.must_equal url
+  end
+end
