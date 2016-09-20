f9f0692bf7dcee9fe86692b0648134ffc2a4c1c2
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-09-20 15:53:19 +0100

    Use vnu-checker to validate the home page
    Errors corrected:
    - Attribute "placeholder" not allowed on element "select" at this point. At line 86, column 189
    - Attribute "autocorrect" not allowed on element "select" at this point. At line 86, column 189
    - Attribute "autocomplete" not allowed on element "select" at this point. At line 86, column 189
    - & did not start a character reference. (& probably should have been escaped as &amp;.) At line 1140, column 93
    - & did not start a character reference. (& probably should have been escaped as &amp;.) At line 1140, column 123
    - & did not start a character reference. (& probably should have been escaped as &amp;.) At line 1140, column 139
    - & did not start a character reference. (& probably should have been escaped as &amp;.) At line 1152, column 90
    - & did not start a character reference. (& probably should have been escaped as &amp;.) At line 1152, column 114
    - & did not start a character reference. (& probably should have been escaped as &amp;.) At line 1152, column 130
    - & did not start a character reference. (& probably should have been escaped as &amp;.) At line 1159, column 219
    - & did not start a character reference. (& probably should have been escaped as &amp;.) At line 1159, column 256
    - & did not start a character reference. (& probably should have been escaped as &amp;.) At line 1159, column 272

---
+++ t/web/html_validation.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require 'test_helper'
+require_relative '../../app'
+require 'vnu'
+
+describe 'HTML validation' do
+  it 'has no errors in the home page' do
+    get '/'
+    validate(last_response.body).must_be_nil
+  end
+
+  def validate(html)
+    Vnu.validate(html, errors_only: true, format: 'json')
+  end
+end
