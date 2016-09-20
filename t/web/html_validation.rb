4c0b2e193c78e92861e155e11ecc9c8c2d649b80
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-09-20 16:26:00 +0100

    Use vnu-checker to validate the house/download page
    Errors corrected:
    - No errors

--- t/web/html_validation.rb
+++ t/web/html_validation.rb
@@ -14,6 +14,11 @@ describe 'HTML validation' do
     validate(last_response.body).must_be_nil
   end
 
+  it 'has no errors in the house/download page' do
+    get '/finland/eduskunta/download.html'
+    validate(last_response.body).must_be_nil
+  end
+
   def validate(html)
     Vnu.validate(html, errors_only: true, format: 'json')
   end
