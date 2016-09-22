2337c515e865f609812fce76b7d78851ce4d77e9
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-09-22 12:27:36 +0100

    Use tidy-html5 to validate the countries page
    Errors corrected:
     - No errors

---
+++ t/web/countries.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'test_helper'
+require_relative '../../app'
+
+describe 'Countries' do
+  describe 'HTML validation' do
+    it 'has no errors in the countries page' do
+      skip if `which tidy`.empty?
+      get '/countries.html'
+      last_response_must_be_valid
+    end
+  end
+end
