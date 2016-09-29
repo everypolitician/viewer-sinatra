e8dc4cc5f44d624cf73fad40109f22026598bbbb
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-09-29 16:15:32 +0100

    Use the HTML helper in the term table template
    The unescaping of the urls at the page level was delivering urls to
    the template that were malformed. Using the helper we can use
    the url as it comes from the data for linking, but unescaped at the
    template level for displaying.

---
+++ t/web/term_table/bahamas.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require 'test_helper'
+require_relative '../../../app'
+
+describe 'Bahamas' do
+  subject { Nokogiri::HTML(last_response.body) }
+
+  before do
+    stub_popolo('4da60b8', 'Bahamas/House_of_Assembly')
+    get '/bahamas/house-of-assembly/term-table/2012.html'
+  end
+
+  describe 'source urls' do
+    let(:sources) { subject.css('.source-credits p:first-child') }
+
+    it 'links to a valid url' do
+      sources.css('a').last.text.must_include '/Members of Parliament/'
+    end
+
+    it 'displays an unescaped url' do
+      sources.css('a/@href').last.text.must_include '/Members+of+Parliament/'
+    end
+  end
+end
